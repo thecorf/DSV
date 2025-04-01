31ae20bed49ca5e7b2d06e36fa1d18e15a63f913
Frederick Grønbech Kure Ertel <fre006@edu.zealand.dk>
2025-04-01 09:50:14 +0200

    Ekstra pages

---
+++ DSV/Pages/CalenderPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DSV.Pages
+{
+    public class CalenderPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
