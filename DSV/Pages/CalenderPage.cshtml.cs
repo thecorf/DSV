7cf4b7522883d24143647e75622a10ebdb69969e
thecorf <mim010@edu.zealand.dk>
2025-04-02 11:32:29 +0200

    More things

--- DSV/Pages/CalenderPage.cshtml.cs
+++ DSV/Pages/CalenderPage.cshtml.cs
@@ -8,6 +8,15 @@ namespace DSV.Pages
 {
     public class CalenderPageModel : PageModel
     {
+        private CalenderService _service;
+        [BindProperty]
+        public List<Array> calenders
+        {  get; set; }
+        public CalenderPageModel(CalenderService serviceCalender) 
+        { 
+            calenders = new List<Array>();
+            _service = serviceCalender;
+        }
         public void OnGet()
         {
 
